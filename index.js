1ac36ad373552a0dd2c1c1d4cce71a6e1701a56e
codevagabond <phung.linh2010@gmail.com>
2021-11-08 10:23:50 +0100

    WIP

--- index.js
+++ index.js
@@ -2,47 +2,46 @@ const robot = require('robotjs');
 
 
 // The middle of the number 1 in numpad
-const NUMAPAD_CALIBRATION_XY = {x: 696, y:348}
-const ENTER_BUTTON = {x: 768, y: 457}
-const DISTANCE_BEETWEEN_NUMBERS = 100; // Assume horizontal and vertical distances are the same
+const NUMAPAD_CALIBRATION_XY = { x: 696, y: 350 }
+const ENTER_BUTTON = { x: 768, y: 500 }
+const DISTANCE_BEETWEEN_NUMBERS = 72; // Assume horizontal and vertical distances are the same
 const PIN_MAX = 999999;
-const START_AT = 0;
-const END_AT = 9;
+const START_AT = 38280;
+const END_AT = 50000;
 
 
 const numpadPositions = generateNumpadPositions();
 
 
-// click(131999);
-// click(12)
-// click(1000)
+// _clickDigit(0)
 start()
+console.log(robot.getMousePos())
 
 
 function start() {
-    for (let i = START_AT; i <END_AT; i++) {
+    for (let i = START_AT; i <= END_AT; i++) {
         clickEnter();
-        sleep(200);
+        sleep(600);
         click(i);
-        sleep(200);
+        sleep(600);
     }
 }
 
 function clickEnter() {
-    const {x, y} = ENTER_BUTTON;
+    const { x, y } = ENTER_BUTTON;
     robot.moveMouse(x, y);
     robot.mouseClick();
 }
 
 function click(number) {
-        if (number <= PIN_MAX) {
-            const clickSequence = generateClickSequence(number)
-            console.log('Inputting PIN ', clickSequence)
-            for (let i = 0; i < clickSequence.length; i++){
-                const c = clickSequence.charAt(i);
-                _clickDigit(number)
-            }
+    if (number <= PIN_MAX) {
+        const clickSequence = generateClickSequence(number)
+        console.log('Inputting PIN ', clickSequence)
+        for (let i = 0; i < clickSequence.length; i++) {
+            const c = clickSequence.charAt(i);
+            _clickDigit(c)
         }
+    }
 }
 
 function _clickDigit(number) {
@@ -55,7 +54,7 @@ function generateClickSequence(number) {
     let numberStr = number.toString();
     const missingZerosCount = PIN_MAX.toString().length - numberStr.length;
     for (let i = 0; i < missingZerosCount; i++) {
-        numberStr = "0" + numberStr; 
+        numberStr = "0" + numberStr;
     }
     return numberStr;
 }
@@ -66,10 +65,12 @@ function sleep(ms) {
 
 
 function generateNumpadPositions() {
-    const numpad = {1: {
-        x: NUMAPAD_CALIBRATION_XY.x,
-        y: NUMAPAD_CALIBRATION_XY.y
-    }};
+    const numpad = {
+        1: {
+            x: NUMAPAD_CALIBRATION_XY.x,
+            y: NUMAPAD_CALIBRATION_XY.y
+        }
+    };
     numpad[2] = {
         x: NUMAPAD_CALIBRATION_XY.x + DISTANCE_BEETWEEN_NUMBERS,
         y: NUMAPAD_CALIBRATION_XY.y
@@ -97,11 +98,11 @@ function generateNumpadPositions() {
     numpad[8] = {
         x: NUMAPAD_CALIBRATION_XY.x + DISTANCE_BEETWEEN_NUMBERS,
         y: NUMAPAD_CALIBRATION_XY.y + (DISTANCE_BEETWEEN_NUMBERS * 2)
-    },
+    }
     numpad[9] = {
         x: NUMAPAD_CALIBRATION_XY.x + (DISTANCE_BEETWEEN_NUMBERS * 2),
         y: NUMAPAD_CALIBRATION_XY.y + (DISTANCE_BEETWEEN_NUMBERS * 2)
-    },
+    }
     numpad[0] = {
         x: numpad[8].x,
         y: numpad[8].y + DISTANCE_BEETWEEN_NUMBERS
